f7c1dfbce3423efc2ee8df1728e69c48e118f243
Enoch Abiodun <117421820+G00380316@users.noreply.github.com>
2025-11-27 15:42:17 +0000

    Moving to Macbook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,16 @@
 import { StyleSheet } from 'react-native';
+import { defaultSystemFonts } from "react-native-render-html";
 import { LogoWave } from '@/components/Bible';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import Animated from 'react-native-reanimated';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { RenderHTML } from 'react-native-render-html';
 import { useWindowDimensions } from 'react-native';
 import { useEffect, useState } from 'react';
+import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
 
 export default function HomeScreen() {
     const [text, setText] = useState("");
-    // const [ssText, setssText] = useState("");
-    // const [ssTitle, setssTitle] = useState("");
     const [title, setTitle] = useState("");
     const [date, setDate] = useState("");
     const [bibleRef, setbibleRef] = useState("");
@@ -18,6 +18,8 @@ export default function HomeScreen() {
     const [audio, setAudio] = useState(null);
 
     const { width } = useWindowDimensions();
+    const bottom = useBottomTabOverflow();
+    const systemFonts = [...defaultSystemFonts, 'Arial', 'Times New Roman'];
 
     useEffect(() => {
         async function load() {
@@ -36,9 +38,9 @@ export default function HomeScreen() {
     }, []);
 
     return (
-        <ParallaxScrollView
-            headerBackgroundColor={{ light: "#ffffffff", dark: "#000000ff" }}
-            headerTitle={
+        <ThemedView style={styles.container}>
+            <Animated.ScrollView
+                contentContainerStyle={{ paddingTop: bottom, paddingBottom: bottom }}>
                 <ThemedView style={styles.titleContainer}>
                     <ThemedText
                         lightColor={"#000000ff"}
@@ -49,75 +51,104 @@ export default function HomeScreen() {
                     </ThemedText>
                     <LogoWave />
                 </ThemedView>
-            }
-        >
-            <RenderHTML
-                contentWidth={width}
-                source={{
-                    html: `
+
+                <ThemedView style={styles.content}>
+                    <RenderHTML
+                        systemFonts={systemFonts}
+                        contentWidth={width}
+                        source={{
+                            html: `
                       <h2>${title}</h2>
                       ${audio}
                       <p class="date">${date}</p>
                       <p class="bibleRef">${bibleRef}</p>
-                      <p class="byline">${byline}</p>
+                      <p class="byline" styles={color:"blue"}>${byline}</p>
                       <p class="text">${text}</p>
                       <br/><br/>
                     `
-                }}
-                tagsStyles={{
-                    h2: {
-                        marginTop: 3,
-                        alignSelf: "center",
-                        color: "black",
-                        fontSize: 22,
-                        fontWeight: "bold",
-                    },
-                    p: {
-                        marginVertical: 4,
-                        fontSize: 14,
-                        color: "black",
-                    },
-                    ".date": {
-                        fontSize: 12,
-                        color: "navy",
-                        textAlign: "center",
-                    },
-                    ".bibleRef": {
-                        color: "black",
-                        textAlign: "center",
-                    },
-                    ".byline": {
-                        color: "blue",
-                        fontStyle: "italic",
-                        textAlign: "center",
-                    },
-                    ".text": {
-                        marginTop: 10,
-                    },
-                    div: {
-                        alignSelf: "center",
-                    },
-                    br: {
-                        height: 20,
-                    },
-                }}
-            />
-        </ParallaxScrollView>
+                        }}
+                        tagsStyles={{
+                            h2: {
+                                alignSelf: "center",
+                                color: "black",
+                                fontSize: 22,
+                                fontWeight: "bold",
+                            },
+                            p: {
+                                marginVertical: 4,
+                            },
+                            div: {
+                                alignSelf: "center",
+                            },
+                            br: {
+                                height: 20,
+                            },
+                        }}
+                        classesStyles={{
+                            date: {
+                                marginVertical: 4,
+                                fontSize: 12,
+                                color: "blue",
+                                textAlign: "center",
+                            },
+                            bibleRef: {
+                                color: "navy",
+                                textAlign: "center",
+                                marginVertical: 4,
+                                fontSize: 14,
+
+                            },
+                            byline: {
+                                marginVertical: 4,
+                                color: "blue",
+                                fontStyle: "italic",
+                                textAlign: "center",
+                                fontSize: 14,
+                            },
+                            text: {
+                                marginTop: 10,
+                            },
+                        }}
+                        renderersProps={{
+                            text: {
+                                allowFontScaling: true,
+                            },
+                        }}
+                        defaultTextProps={{
+                            selectable: false,
+                        }}
+                    />
+                </ThemedView>
+            </Animated.ScrollView>
+        </ThemedView>
     );
 }
 
 const styles = StyleSheet.create({
     titleContainer: {
-        height: 190,
         width: '100%',
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
         backgroundColor: "transparent",
-        padding: 15
+        paddingLeft: 15,
+        paddingRight: 15,
     },
     stepContainer: {
         gap: 8,
         marginBottom: 8,
     },
+    container: {
+        flex: 1,
+    },
+    header: {
+        overflow: 'hidden',
+    },
+    content: {
+        flex: 1,
+        padding: 32,
+        gap: 16,
+        overflow: 'hidden',
+    },
 });
+
--- app/(tabs)/sundaySchool.tsx
+++ app/(tabs)/sundaySchool.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-import { HelloWave } from "@/components/Bible";
+import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
